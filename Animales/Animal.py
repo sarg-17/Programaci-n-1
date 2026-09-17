3ac248f5eb90df37b7d8222103f4b718f87ece97
sarg-17 <samanthaariosg@gmail.com>
2026-09-17 09:31:49 -0400

    update Animales/Animal.py

---
+++ Animales/Animal.py
@@ -0,0 +1,7 @@
+class Animal:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def hacer_sonido(self):
+        print("Este animal hace un sonido genérico.")

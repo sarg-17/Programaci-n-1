b98bf685139d210f1e8633f7c6f4a82f6eb58022
sarg-17 <samanthaariosg@gmail.com>
2026-09-17 15:47:22 -0400

    create Prácticas/Estudiante/Main.py

---
+++ Prácticas/Estudiante/Main.py
@@ -0,0 +1,8 @@
+from Persona import Persona
+from Estudiante import Estudiante
+
+personal=Persona("Juan", 30)
+print(personal.saludar())
+
+estudiantel=Estudiante("Ana", 22, "Matemáticas")
+print(estudiantel.info())

b89595a5dade7926f544a889e307f83b901c8920
sarg-17 <samanthaariosg@gmail.com>
2026-09-17 09:57:24 -0400

    update Ejercicios/Sensores.py

--- Ejercicios/Sensores.py
+++ Ejercicios/Sensores.py
@@ -1,3 +1,3 @@
-sonsores=["Temperatura", "Distancia", "Presión", "Humedad"]
+sensores=["Temperatura", "Distancia", "Presión", "Humedad"]
 for i, sensor in enumerate(sensores):
   print(f"{i}: {sensor}")
